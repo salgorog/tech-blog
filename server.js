d8f123a94c6e43c6c496a5f48391dd52816fb556
Aztec <salgorog@hotmail.com>
2020-11-01 23:13:06 -0700

    server running

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const sess = {
-    secret: 'Super secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({ db: sequelize })
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
-const hbs = exphbs.create( { helpers } );
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,5 +35,5 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
