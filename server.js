be5426110a07921d8123c8e1bc80d054a794f009
Aztec <salgorog@hotmail.com>
2020-11-02 23:33:14 -0700

    server OD running

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
 
 // turn on routes
 app.use(routes);
